c9f56c4257db09decbdc7433db98efe40a37c053
MitchellWatson <90720653+MitchellWatson@users.noreply.github.com>
2021-11-01 20:53:02 -0700

    Add files via upload

---
+++ FitMealJavascript.js
@@ -0,0 +1,10 @@
+var  firebaseConfig = {
+    apiKey: "AIzaSyDME08aOUZ7f_ZCKlMGZlp6L2-TBu6YygQ",
+    authDomain: "bby06-a573c.firebaseapp.com",
+    projectId: "bby06-a573c",
+    storageBucket: "bby06-a573c.appspot.com",
+    messagingSenderId: "12753689349",
+    appId: "1:12753689349:web:6b850b66ebc4d7d325b56c"
+    };
+  
+    const app = firebase.initializeApp(firebaseConfig);

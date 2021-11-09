d45f4ea8648cf7909eb9a3767794fcd54466b707
Sunreet <ssidhu261@my.bcit.ca>
2021-11-09 01:54:59 -0800

    firebase authentication done

--- scripts/my_script.js
+++ scripts/my_script.js
@@ -1,3 +1,17 @@
+// Fitmeal's firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAwKw9nxIDn3tfJm3qiI2GJV2d_YuYwn6w",
+    authDomain: "fitmeal-df106.firebaseapp.com",
+    projectId: "fitmeal-df106",
+    storageBucket: "fitmeal-df106.appspot.com",
+    messagingSenderId: "717872291044",
+    appId: "1:717872291044:web:0aa1649cf10ca9e44a60f1"
+  };
+
+   // Initialize Firebase
+   const app = firebase.initializeApp(firebaseConfig);
+
+
 function sayHello() {
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
